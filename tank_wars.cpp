d902971f95aeaf37207ae5b4bfb0440cdfe6da0a
Horia Moroianu <moroianu.horiav@gmail.com>
2024-11-03 10:57:59 +0200

    Add project base files
    Signed-off-by: Horia Moroianu <moroianu.horiav@gmail.com>

---
+++ tank_wars.cpp
@@ -0,0 +1,92 @@
+#include "lab_m1/tank-wars-2024/tank_wars.h"
+
+using namespace m1;
+
+
+TankWars::TankWars()
+{
+}
+
+
+TankWars::~TankWars()
+{
+}
+
+
+void TankWars::Init()
+{
+    glm::ivec2 resolution = window->GetResolution();
+    auto camera = GetSceneCamera();
+    camera->SetOrthographic(0, (float)resolution.x, 0, (float)resolution.y, 0.01f, 400);
+    camera->SetPosition(glm::vec3(0, 0, 50));
+    camera->SetRotation(glm::vec3(0, 0, 0));
+    camera->Update();
+    GetCameraInput()->SetActive(false);
+}
+
+
+void TankWars::FrameStart()
+{
+    // Clears the color buffer (using the previously set color) and depth buffer
+    glClearColor(0.6f, 0.6f, 0.6f, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glm::ivec2 resolution = window->GetResolution();
+    // Sets the screen area where to draw
+    glViewport(0, 0, resolution.x, resolution.y);
+}
+
+
+void TankWars::Update(float deltaTimeSeconds)
+{
+}
+
+
+void TankWars::FrameEnd()
+{
+}
+
+
+void TankWars::OnInputUpdate(float deltaTime, int mods)
+{
+}
+
+
+void TankWars::OnKeyPress(int key, int mods)
+{
+    // Add key press event
+}
+
+
+void TankWars::OnKeyRelease(int key, int mods)
+{
+    // Add key release event
+}
+
+
+void TankWars::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY)
+{
+    // Add mouse move event
+}
+
+
+void TankWars::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods)
+{
+    // Add mouse button press event
+}
+
+
+void TankWars::OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods)
+{
+    // Add mouse button release event
+}
+
+
+void TankWars::OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY)
+{
+}
+
+
+void TankWars::OnWindowResize(int width, int height)
+{
+}
